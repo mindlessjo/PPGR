3c6f48cf056712bfa77d0e2f95a8aa58be53ce46
mindl <mindlessmuseum@gmail.com>
2025-02-20 20:28:34 -0500

    updating Calculator

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+int main() {
+
+    cout << "Calculator V1.0." << endl << endl;
+
+    int firstArgument;
+    cout << "First Argument: ";
+    cin >> firstArgument;
+
+    char operatorArgument;
+    cout << "Operator Argument: ";
+    cin >> operatorArgument;
+
+    int secondArgument;
+    cout << "Second Argument: ";
+    cin >> secondArgument;
+
+    int sum;
+    switch (operatorArgument) {
+        case '+': sum = firstArgument + secondArgument; break;
+        case '-': sum = firstArgument - secondArgument; break;
+        case '*': sum = firstArgument * secondArgument; break;
+        case '/': sum = firstArgument / secondArgument; break;
+        default: cout << "Invalid operator argument." << endl;
+        return 1;
     }
+
+    cout << "Equation: " << firstArgument << " " << operatorArgument << " " << secondArgument << endl;
+    cout << "Result: " << sum << endl;
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
