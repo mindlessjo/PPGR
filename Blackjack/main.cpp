88f882379ab956136b2cb695c075aab3a3e909a3
mindl <mindlessmuseum@gmail.com>
2025-02-20 21:03:53 -0500

    updating blackjack

---
+++ Blackjack/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+
+//void reveal_hands(dealer_score, player_score, bust_score){
+//
+//};
+
+int main() {
+    cout << "Blackjack V2.0\n\n"
+    << "Rules:\n"
+    << "Type 'hit' to recieve another card. \n"
+    << "Type 'stand' to to stop and reveal hands\n";
+
+
+
+    return 0;
+}
