18bdf7fc7937b0c1470373ffdad69c6657c2cfb4
Joseph Maddox <mindlessmuseum@gmail.com>
2025-03-16 20:29:56 -0400

    Temperature Conversion Start

---
+++ Temperature Converter C/cmake-build-debug/CMakeFiles/Temperature_Converter_C.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Temperature_Converter_C.
+# This may be replaced when dependencies are built.
--- Temperature Converter C/main.c
+++ Temperature Converter C/main.c
@@ -1,18 +1,69 @@
 #include <stdio.h>
 
+int conversion(double currentTemp, char currentUnit, char targetUnit) {
+
+    float result;
+
+    return 1+1;
+}
+
 int main(void) {
     printf("Temperature Converter 2.0\n");
 
-    float currentTemp;
+    double currentTemp;
     char currentUnit, targetUnit;
+    int validInput;
 
-    printf("Enter temperature: ");
-    scanf("%f", currentTemp);
+    do {
+        validInput = 0;
+        printf("\nEnter temperature: ");
 
-    printf("Enter temperature unit");
-    scanf("%c",currentUnit);
+        if (scanf("%lf", &currentTemp) == 1) {
+            validInput = 1;
+        } else {
+            printf("Incorrect value");
+        }
 
-    print
+        while (getchar() != '\n');
+    } while (!validInput);
+
+    do {
+        validInput = 0;
+        printf("\nEnter current unit: ");
+        scanf(" %c", &currentUnit);
+
+        if (currentUnit == 'F' || currentUnit == 'C' || currentUnit == 'K') {
+            validInput = 1;
+        } else {
+            printf("Incorrect value");
+        }
+
+        while (getchar() != '\n');
+    } while (!validInput);
+
+    do {
+        validInput = 0;
+        printf("\nEnter target unit: ");
+        scanf(" %c", &targetUnit);
+
+        if (targetUnit == 'F' || targetUnit == 'C' || targetUnit == 'K') {
+            validInput = 1;
+        } else {
+            printf("Incorrect value");
+        }
+
+        while (getchar() != '\n');
+    } while (!validInput);
+
+    double targetTemp = conversion(currentTemp, currentUnit, targetUnit);
+
+    printf("Temperature: %0.2lf \n"
+    "Current Unit: %c \n"
+    "Target Unit: %c \n"
+    "Final Temperature: %0.2lf \n\n",
+    currentTemp, currentUnit, targetUnit, targetTemp
+    );
+
 
     return 0;
 }
