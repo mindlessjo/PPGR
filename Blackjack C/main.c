ddb36c79d1e39c3314e75bc87732c0acd94bbadd
Joseph Maddox <mindlessmuseum@gmail.com>
2025-03-18 22:12:05 -0400

    gghj

--- Blackjack C/main.c
+++ Blackjack C/main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+int deal(int playerHand,int opponentHand) {
+
+    return (playerHand, opponentHand);
+}
+
+void scoreCompare() {
+
+};
 
 int main(void) {
     printf("Blackjack V2.0!\n");
-    printf("Enter 'hit' or 'stand' respectively");
+    printf("Enter 'hit' or 'stand' respectively\n\n");
+
+    int inputValue = 0;
+
 
     do {
+        char userInput[5];
+        int playerHand;
+        int opponentHand;
+        int bust = 21;
 
+        printf("Hit or Stand?: ");
+        scanf("%s", userInput);
 
-    } while ();
+        if (strcmp(userInput, "hit") == 0) {
+            inputValue = 1;
+        } else if (strcmp(userInput, "stand") == 0) {
+            inputValue = 0;
+        }
+    } while (inputValue);
+
 
     return 0;
 }
