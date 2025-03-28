0a187de525318c1cff057f7290268081e289439c
Joseph Maddox <mindlessmuseum@gmail.com>
2025-03-28 13:00:52 -0400

    update

--- Blackjack C/main.c
+++ Blackjack C/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int deal(int playerHand,int opponentHand) {
 
-    return (playerHand, opponentHand);
+// function for dealing cards to players hand
+int deal(int playerHand) {
+
+
+    return 1;
 }
 
+// function for win condition and score comparison
 void scoreCompare() {
 
 };
 
+//main function handling looping
 int main(void) {
     printf("Blackjack V2.0!\n");
     printf("Enter 'hit' or 'stand' respectively\n\n");
 
-    int inputValue = 0;
+    int inputValue = 0;     // Checks user input for loop condition
+
 
 
     do {
@@ -26,10 +32,16 @@ int main(void) {
         printf("Hit or Stand?: ");
         scanf("%s", userInput);
 
+        // Checks if comparison between user input and the specified string is true
+        //strcmp true is 0
         if (strcmp(userInput, "hit") == 0) {
+            printf(deal(playerHand));
             inputValue = 1;
         } else if (strcmp(userInput, "stand") == 0) {
             inputValue = 0;
+        } else {
+            printf("Please enter \"hit\" or \"stand\" \n");
+            inputValue = 1;
         }
     } while (inputValue);
 
---
+++ Blackjack/cmake-build-debug/CMakeFiles/Blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Blackjack.exe"
+  "Blackjack.exe.manifest"
+  "Blackjack.pdb"
+  "CMakeFiles/Blackjack.dir/main.cpp.obj"
+  "CMakeFiles/Blackjack.dir/main.cpp.obj.d"
+  "libBlackjack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
