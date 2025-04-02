63b2e5d99b4ef40c560baeda107d82ee6904f3d7
mindl <mindlessmuseum@gmail.com>
2025-04-02 19:58:04 -0400

    Finished

--- User Login System/main.c
+++ User Login System/main.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+void clearstdin() {
+    int x;
+    while ((x = getchar()) != '\n' && x != EOF);
+}
 
 int main(void) {
     printf("User Login Password\n");
 
+
     char username[15];
-    printf("Enter Username (14 character maximum) :");
+    printf("Enter Username (14 character maximum): ");
     fgets(username, sizeof(username), stdin);
-    getchar();
+
+    if (username[strlen(username) - 1] != '\n') {
+        clearstdin();
+    } else {
+        username[strlen(username) - 1] = '\0';
+    };
+
 
     char password[15];
     printf("Enter Password: ");
     fgets(password, sizeof(password), stdin);
-    getchar();
+
+    if (password[strlen(password) - 1] != '\n') {
+        clearstdin();
+    } else {
+        password[strlen(password) - 1] = '\0';
+    };
 
     printf(
            "Login: "
