489bb837dd47ac043a8d4bb05cd8d4b1c433dcf1
Joseph Maddox <mindlessmuseum@gmail.com>
2025-04-02 20:33:18 -0400

    idk this better work

--- Hangman C/main.c
+++ Hangman C/main.c
@@ -1,48 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-// #define
 
 int main(void) {
-    printf("Hangman V1.0!\n");
 
-    // declares user input as the chosen word of the game
-    char inputWord[20];
-    printf("Enter the word for the game: ");
-    scanf("%s", inputWord);
-    getchar();
-
-    // declares user input length of chosen word
-    int lengthofWord = strlen(inputWord);
-
-    // declares show blank spaces for length of word
-    strcat(blankKey[lengthofWord], "");
-    for (int i=0; i < lengthofWord; i++ ) {
-        strcat(blankKey, "_");  // Append "_" to the string
-    }
-
-    // Hangman Guessing Loop
-    int strike = 0;
-
-    do {
-        printf(blankKey);
-
-    // looping input into spaces or counting incorrect inputs
-        for (int i = 0; i < 7; i++) {
-
-            printf("7 Guesses to end loop");
-            strike++;
-            i++;
-        }
-
-
-    } while (strcmp(blankKey, inputWord) != 0 || strike == 7); // compares the character arrays and the strike limit
-
-
-    printf("THIS IS NOT A DRILL");
-    printf("THIS IS A DRILL ACTUALLY");
-    printf("LAHEY");
-    printf("why you gtta be that way???");
-    printf("my laptop");
     return 0;
 }
